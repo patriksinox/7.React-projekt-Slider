1116142b7a63713b6ae0605c80eb77a872d79626
patriksinox <patrik.subjak1502@gmail.com>
2022-10-16 17:46:00 +0200

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import { FcNext, FcPrevious } from "react-icons/fc";
+import { RiDoubleQuotesL } from "react-icons/ri";
+import people from "./data";
 
 function App() {
+  const [ludia, setLudia] = useState(people);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (index > ludia.length - 1) {
+      setIndex(0);
+      return;
+    }
+    if (index < 0) {
+      setIndex(ludia.length - 1);
+      return;
+    }
+  }, [index, ludia]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => clearInterval(slider);
+  }, [index]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="titul">
+        <h1 className="text-center mt-5">Project-Slider</h1>
+        <div className="underline"></div>
+      </div>
+      <section className=".container text-center">
+        {ludia.map((clovek, clovekIndex) => {
+          const { image, name, title, quote } = clovek;
+          let pozicia = "nextSlide";
+          if (clovekIndex === index) {
+            pozicia = "aktivSlide";
+          }
+          if (
+            clovekIndex === index - 1 ||
+            (index === 0) & (clovekIndex === ludia.length - 1)
+          ) {
+            pozicia = "lastSlide";
+          }
+          return (
+            <article key={clovek.id} className={pozicia}>
+              <img src={image} alt={name} />
+
+              <h4>{name}</h4>
+              <h6>{title}</h6>
+              <p>{quote}</p>
+              <span>
+                <RiDoubleQuotesL className="kvacka" />
+              </span>
+
+              <FcPrevious
+                className="prev"
+                onClick={() => setIndex(index - 1)}
+              />
+
+              <FcNext className="next" onClick={() => setIndex(index + 1)} />
+            </article>
+          );
+        })}
+      </section>
+    </>
   );
 }
 
---
+++ src/Popis.js
@@ -0,0 +1,18 @@
+function Popis() {
+  return (
+    <>
+      <div className="container text-center">
+        <h1 className="mt-5">Môj Siedmy React projekt-Slider</h1>
+        <p className="popisProjektu">
+          <strong>React.JS</strong> a {""}
+          <strong>Bootstrap</strong> projekt. Projekt je podľa predlohy
+          inštruktora <strong>Johna Smilgu</strong> z <strong>Udemy</strong>.
+          Návrh projektu je jeho. Kód je môj 😊. V Projekte som použil Vanilla
+          JS s Reactom - useState, useEffect a React Ikonky.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Popis;
